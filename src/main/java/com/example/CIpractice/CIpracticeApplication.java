e3d1ff610ccd729a4aa6c1dbd01b014f6b52b060
DhanushGowdaM <dhanushgowdam2003@gmail.com>
2026-03-31 12:07:57 +0530

    First commit

---
+++ src/main/java/com/example/CIpractice/CIpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.CIpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CIpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CIpracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CIpractice/CIpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CIpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CIpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
